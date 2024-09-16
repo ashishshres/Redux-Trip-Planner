512eea4e73c3856bbd37362f3583538fc54999ed
ashishshres <ashishshr27@gmail.com>
2024-09-16 17:02:35 +0545

    fix: update import path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./App/store.js";
+import { store } from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>
